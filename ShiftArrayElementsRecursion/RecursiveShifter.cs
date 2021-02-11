f2a217ea7db6928662ff940e42f16327eaaf2a54
Alexander Rykov <154324-alexander-rykov@users.noreply.gitlab.com>
2021-02-11 12:43:33 +0300

    Add solution configuration files: editor configs, stylecop settings, style ruleset, readme. Add projects with unit tests.

---
+++ ShiftArrayElementsRecursion/RecursiveShifter.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace ShiftArrayElements
+{
+    public static class RecursiveShifter
+    {
+        /// <summary>
+        /// Shifts elements in a <see cref="source"/> array using <see cref="iterations"/> array for getting directions and iterations (odd elements - left direction, even elements - right direction).
+        /// </summary>
+        /// <param name="source">A source array.</param>
+        /// <param name="iterations">An array with iterations.</param>
+        /// <returns>An array with shifted elements.</returns>
+        /// <exception cref="ArgumentNullException">source array is null.</exception>
+        /// <exception cref="ArgumentNullException">iterations array is null.</exception>
+        public static int[] Shift(int[] source, int[] iterations)
+        {
+            // TODO #2. Implement the method using recursive local functions and indexers only (don't use Array.Copy method here).
+            throw new NotImplementedException();
+       }
+    }
+}
